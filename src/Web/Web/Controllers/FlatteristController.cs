96706f6b747551b389e1bdc265218cf564569ab4
Wade Wegner <wade.wegner@gmail.com>
2015-07-19 13:14:51 -0700

    Working prototype

---
+++ src/Web/Web/Controllers/FlatteristController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace Web.Controllers
+{
+    public class Compliment
+    {
+        public string HashKey { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class FlatteristController : ApiController
+    {
+        static readonly Random Random = new Random();
+        const string ComplimentsUri = @"http://www.flatterist.com/compliments.json";
+
+        public Compliment Get()
+        {
+            var json = new WebClient().DownloadString(ComplimentsUri);
+
+            dynamic stuff = JsonConvert.DeserializeObject(json);
+            var compliments = new List<Compliment>();
+
+            foreach (var blah in stuff.compliments)
+            {
+                compliments.Add(new Compliment { HashKey = blah[0], Description = blah[1] });
+            }
+
+            var index = Random.Next(compliments.Count);
+
+            return compliments[index];
+        }
+    }
+}
---
+++ src/Web/Web/Controllers/TwilioController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Twilio;
+
+namespace Web.Controllers
+{
+    public class TwilioController : ApiController
+    {
+        public string Get([FromUri] string phoneNumber, string flatteristId, string flattery)
+        {
+            const string accountSid = "AC4a8be98fbae816067e0c0517858f56fd";
+            const string authToken = "03e552278c505744bb862fa53e24b073";
+            const string fromNumber = "+12245883104";
+
+            var client = new TwilioRestClient(accountSid, authToken);
+
+            if (string.IsNullOrEmpty(flattery))
+            {
+
+
+                var call = client.InitiateOutboundCall(
+                    fromNumber, // The number of the phone initiating the call
+                    string.Format("+{0}", phoneNumber), // The number of the phone receiving call
+                    string.Format("http://www.flatterist.com/{0}.mp3", flatteristId)
+                    // The URL Twilio will request when the call is answered
+                    );
+
+                if (call.RestException == null)
+                {
+                    return string.Format("Started call: {0}", call.Sid);
+                }
+                return string.Format("Error: {0}", call.RestException.Message);
+            }
+
+            client.SendMessage(fromNumber, phoneNumber, flattery);
+
+            return string.Format("Sent message to {0}", phoneNumber);
+        }
+    }
+}
